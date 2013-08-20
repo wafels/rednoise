fixed normalization of power laws

py/testing/test_posterior_predictive.py
@@ -15,15 +15,14 @@ from pymcmodels import single_power_law_with_constant
 # Create some fake data
 dt = 12.0
 nt = 300
-pls1 = SimplePowerLawSpectrumWithConstantBackground([10.0, 2.0, -5.0], nt=nt, dt=dt)
+pls1 = SimplePowerLawSpectrumWithConstantBackground([10.0, 1.0, -5.0], nt=nt, dt=dt)
 data = TimeSeriesFromPowerSpectrum(pls1).sample
 
+# Create a time series object
 ts = TimeSeries(dt * np.arange(0, nt), data)
 
-filename = os.path.expanduser('~/Desktop/test.pickle')
-
 # Analyze using MCMC
-result = Do_MCMC([ts]).okgo(single_power_law_with_constant, iter=50000, burn=10000, thin=5, progress_bar=False)
+analysis = Do_MCMC([ts]).okgo(single_power_law_with_constant, iter=50000, burn=10000, thin=5, progress_bar=False)
 
 
 # Do the posterior predictive statistics to measure GOF
@@ -72,17 +71,25 @@ def posterior_predictive_distribution(iobs, fit_results,
         ts = TimeSeries(dt * np.arange(0, nt), data)
 
         # Get the simulated data's power spectrum
-        S = ts.PowerSpectrum.ppower
+        S = ts.PowerSpectrum.Npower
 
+        # Calculate the required discrepancy statistic 
         if statistic == 'vaughan_2010_T_R':
             value = vaughan_2010_T_R(iobs, S)
         if statistic == 'vaughan_2010_T_SSE':
             value = vaughan_2010_T_R(iobs, S)
         distribution.append(value)
+
     return np.array(distribution)
 
-fit_results = result.results[0]["samples"]
+# Get the MCMC results from the analysis
+fit_results = analysis.results[0]["samples"]
 
+str(list(mp.variables)[0].__name__)
+
+
+# Calculate the posterior predictive distribution
 x = posterior_predictive_distribution(ts.PowerSpectrum.ppower, fit_results)
 
-value = vaughan_2010_T_R(ts.PowerSpectrum.ppower/ts.PowerSpectrum.ppower[0], pls1.power()/pls1.power()[0])
+# Calculate the discrepancy statistic
+value = vaughan_2010_T_R(ts.PowerSpectrum.Npower, )

py/tools/rnfit2.py
@@ -54,26 +54,35 @@ class Do_MCMC:
             # Calculate the power at the positive frequencies
             self.pwr = ts.PowerSpectrum.ppower
             self.fpos = ts.PowerSpectrum.frequencies.positive
-            # Normalize the power
-            power_norm = self.pwr[0]
-            self.pwr = self.pwr / power_norm
+
+            # Following Vaughan (2010): normalize to the mean power
+            power_mean = np.mean(self.pwr)
+            self.pwr = self.pwr / power_mean
+            # Normalize now to the standard deviation
+            power_std = np.std(self.pwr)
+            self.pwr = self.pwr / power_std
+
             # Set up the MCMC model
             self.pymcmodel = pymcmodel(self.fpos, self.pwr)
             self.M = pymc.MCMC(self.pymcmodel)
+
             # Do the MCMC calculation
             self.M.sample(**kwargs)
+
             # Get the samples
             k = self.M.stats().keys()
             samples = {}
             for key in k:
                 if key is not 'fourier_power_spectrum':
                     samples[key] = self.M.trace(key)[:]
+
             # Append the stats results and the samples
             # Get the MAP results
             mp = pymc.MAP(self.pymcmodel)
             mp.fit()
             self.results.append({"timeseries": ts,
-                                 "norm": power_norm,
+                                 "mean": power_mean,
+                                 "std": power_std,
                                  "power": self.pwr,
                                  "frequencies": self.fpos,
                                  "location": k,
@@ -99,7 +108,7 @@ class Do_MCMC:
     def showfit(self, loc=0, figure=2):
         """ Show a spectral fit summary plot"""
         # Construct a summary for each variable
-        k = self.results[0]['stats'].keys()
+        k = self.results[loc]['stats'].keys()
         description = []
         for key in k:
             if key is not 'fourier_power_spectrum':

py/tools/rnsimulation.py
@@ -33,6 +33,9 @@ class PowerSpectrum:
         self.power = power
         self.ppower = self.power[self.frequencies.posindex]
         self.label = 'Fourier power'
+        # normalized power
+        d1 = self.ppower / np.mean(self.ppower)
+        self.Npower = d1 / np.std(d1)
 
 
 class TimeSeries:
@@ -105,7 +108,7 @@ class BrokenPowerLaw(PowerLawPowerSpectrum):
 
 
 class TimeSeriesFromPowerSpectrum():
-    def __init__(self, powerspectrum, V=10, W=10, seed=None, fft_zero=0.0):
+    def __init__(self, powerspectrum, V=10, W=10, seed=None, fft_zero=0.0, **kwargs):
         """
         Create a time series with a given type of power spectrum.  This object
         can be used to generate time series that have a given power spectrum.
@@ -158,7 +161,7 @@ class TimeSeriesFromPowerSpectrum():
 
         # the fully over-sampled timeseries, with a sampling cadence of dt/W, with a
         # duration of V*N*dt
-        self.oversampled = time_series_from_power_spectrum(self.inputpower, fft_zero=self.fft_zero, seed=self.seed)
+        self.oversampled = time_series_from_power_spectrum(self.inputpower, fft_zero=self.fft_zero, seed=self.seed, **kwargs)
 
         # Subsample the time-series back down to the requested cadence of dt
         self.longtimeseries = self.oversampled[0:len(self.oversampled):self.W]
@@ -200,7 +203,7 @@ def noisy_power_spectrum(S, seed=None):
     return S * X / 2.0
 
 
-def time_series_from_power_spectrum(S, fft_zero=0.0, seed=None):
+def time_series_from_power_spectrum(S, fft_zero=0.0, seed=None, no_noise=False):
     """Create a time series with power law noise, following the recipe
     of Vaughan (2010), MNRAS, 402, 307, appendix B
 
@@ -224,11 +227,15 @@ def time_series_from_power_spectrum(S, fft_zero=0.0, seed=None):
     K = len(S)
 
     # noisy power spectrum
-    I = S#noisy_power_spectrum(S, seed=seed)
+    if no_noise:
+        I = S
+    else:
+        I = noisy_power_spectrum(S, seed=seed)
 
     # random phases, except for the nyquist frequency.
     ph = uniform.rvs(loc=-np.pi / 2.0, scale=np.pi, size=K, seed=seed)
-    ph[:] = 0.0
+    if no_noise:
+        ph[:] = 0.0
     ph[-1] = 0.0
 
     # Amplitudes
@@ -242,13 +249,13 @@ def time_series_from_power_spectrum(S, fft_zero=0.0, seed=None):
     F = A * np.exp(-np.complex(0, 1) * ph)
 
     # Form the negative frequency part
-    F_negative = np.conjugate(F)[::-1][1:]
+    #F_negative = np.conjugate(F)[::-1][1:]
 
     # Form the fourier transform
-    F_complete = np.concatenate((np.asarray([fft_zero]), F, F_negative))
+    #F_complete = np.concatenate((np.asarray([fft_zero]), F, F_negative))
 
     # create the time-series.  The complex part should be tiny.
-    T_sim = np.fft.ifft(F_complete)
+    #T_sim = np.fft.ifft(F_complete)
     T_sim = np.fft.irfft(np.concatenate((np.asarray([fft_zero]), F)))
 
     # The time series is formally complex.  Return the real part only.