3a2d2773f9ef0cba8afc33a7f0afe58baf18ec0f
wafels <jackireland@gmail.com>
2014-02-13 19:30:38 -0500

    some cleanup

--- py/tools/cubetools.py
+++ py/tools/cubetools.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy
 import sunpy
 import matplotlib.pyplot as plt
 from scipy.io import readsav
@@ -7,7 +6,6 @@ import os
 from sunpy.coords.util import rot_hpc
 import tsutils
 import pickle
-import itertools
 
 # For image co-registration
 from skimage.feature import match_template
@@ -33,7 +31,7 @@ def derotated_datacube_from_mapcube(maps, ref_index=0, diff_limit=0.01):
     # Output datacube
     datacube = np.zeros((ny, nx, nt))
 
-    # maximum differences found si far
+    # maximum differences found so far
     max_abs_xdiff = 0.0
     max_abs_ydiff = 0.0
 
@@ -60,19 +58,17 @@ def derotated_datacube_from_mapcube(maps, ref_index=0, diff_limit=0.01):
         if np.abs(ydiff) > max_abs_ydiff:
             max_abs_ydiff = np.ceil(np.abs(ydiff))
 
-        #print xdiff, ydiff
         # In matplotlib, the origin of the image is in the top right of the
         # the array.  Therefore, shifting in the y direction has to be reversed
         # Also, the first dimension in the numpy array refers to what a solar
         # physicist refers to as the north-south (i.e., 'y') direction.
-        #print t, xdiff, ydiff
         if np.abs(ydiff) >= diff_limit and np.abs(xdiff) >= diff_limit:
             shifted = shift(m.data, [-ydiff, -xdiff])
         else:
             shifted = m.data
+
         # Store all the shifted data.
         datacube[..., t] = shifted
-        #print t, m.date, maps[t].center, xdiff, ydiff
 
     # Zero out the edges where the data has been moved
     return datacube[0:ny - max_abs_ydiff - 1, 0:nx - max_abs_xdiff - 1:, :]
