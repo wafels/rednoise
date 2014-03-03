3024dc78fef3a3c7257f54718aad3eee19ecd188
wafels <jackireland@gmail.com>
2014-03-02 19:03:16 -0500

    more posterior predictive development

--- py/aia/aia_pymc_pwrlaws.py
+++ py/aia/aia_pymc_pwrlaws.py
@@ -9,9 +9,42 @@ import numpy as np
 
 import pymc
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 
 import aia_specific
+import aia_plaw
 import pymcmodels
+
+#
+# Curvefit the log of the power law plus constant with a Gaussian
+#
+def curve_fit_LogPowerLawPlusConstantGaussian(x, y, sigma, p0=None):
+    A = curve_fit(aia_plaw.LogPowerLawPlusConstantGaussian, x, y, p0=p0)
+    best_fit = aia_plaw.LogPowerLawPlusConstantGaussian(x,
+                                                   A[0][0],
+                                                   A[0][1],
+                                                   A[0][2],
+                                                   A[0][3],
+                                                   A[0][4],
+                                                   A[0][5])
+    likelihood = pymc.normal_like(best_fit, y, 1.0 / (sigma ** 2))
+
+    return A, best_fit, likelihood
+
+
+#
+# Curvefit the log of the power law plus constant with a Gaussian
+#
+def curve_fit_LogPowerLawConstant(x, y, sigma, p0=None):
+    A = curve_fit(aia_plaw.LogPowerLawPlusConstant, x, y, p0=p0)
+    best_fit = aia_plaw.LogPowerLawPlusConstant(x,
+                                                   A[0][0],
+                                                   A[0][1],
+                                                   A[0][2])
+    likelihood = pymc.normal_like(best_fit, y, 1.0 / (sigma ** 2))
+
+    return A, best_fit, likelihood
+
 
 plt.ion()
 #
@@ -25,11 +58,11 @@ corename = '20120923_0000__20120923_0100'
 sunlocation = 'disk'
 fits_level = '1.5'
 waves = ['171']
-regions = ['moss', 'qs', 'loopfootpoints', 'sunspot']
+regions = ['moss']
 windows = ['hanning']
 manip = 'relative'
 
-nsample = 1000
+nsample = 100
 
 for iwave, wave in enumerate(waves):
     # Which wavelength?
@@ -74,7 +107,7 @@ for iwave, wave in enumerate(waves):
                 os.makedirs(savefig)
             savefig = os.path.join(savefig, region_id)
 
-            for obstype in ['.logiobs', '.iobs']:
+            for obstype in ['.logiobs']:
                 #
                 print('Analyzing ' + obstype)
 
@@ -105,6 +138,15 @@ for iwave, wave in enumerate(waves):
                     # Run the sampler
                     print('Running simple power law model')
                     M.sample(iter=50000, burn=10000, thin=5, progress_bar=True)
+                    
+                    # Get the mean parameters as a good guess.
+                    Mstats = M.stats()
+                    p_orig = np.asarray([np.exp(Mstats['power_law_norm']['mean']),
+                                         Mstats['power_law_index']['mean'],
+                                         np.exp(Mstats['background']['mean'])])
+                    
+                    #
+                    _, _, likelihood_best = curve_fit_LogPowerLawConstant(x, pwr, sigma, p0=p_orig)
 
                     # Set up the second model - single power law with constant and Gaussian bump
                     pymcmodel2 = pymcmodels.Log_splwc_GaussianBump(x, pwr, sigma)
@@ -115,6 +157,16 @@ for iwave, wave in enumerate(waves):
                     # Run the sampler
                     print('Running power law plus Gaussian model')
                     M2.sample(iter=50000, burn=10000, thin=5, progress_bar=True)
+                    M2stats = M2.stats()
+                    p2_orig = np.asarray([np.exp(M2stats['power_law_norm']['mean']),
+                                         M2stats['power_law_index']['mean'],
+                                         np.exp(M2stats['background']['mean']),
+                                         M2stats['gaussian_amplitude']['mean'],
+                                         M2stats['gaussian_position']['mean'],
+                                         M2stats['gaussian_width']['mean']])
+                    
+                    #
+                    _, _, likelihood2_best = curve_fit_LogPowerLawPlusConstantGaussian(x, pwr, sigma, p0=p2_orig)
 
    
                 if obstype == '.iobs':
@@ -130,6 +182,8 @@ for iwave, wave in enumerate(waves):
                     # Run the sampler
                     print('Running simple power law model')
                     M.sample(iter=50000, burn=10000, thin=5, progress_bar=True)
+                    
+                    # Get some mean paramters
 
                     # Set up the second model - single power law with constant and Gaussian bump
                     pymcmodel2 = pymcmodels.Log_splwc_GaussianBump_lognormal(x, np.exp(pwr), sigma)
@@ -166,28 +220,37 @@ for iwave, wave in enumerate(waves):
                 #
                 t_lrt_distribution = []
                 for r in range(0, nsample):
+                    print r
+                    #
                     # Data for the first model
-                    M_data = M.trace("predictive")[r]
+                    #
+                    M_pred = M.trace("predictive")[r]
                     
                     # Perform the fit
-                    
-                    # Get the likelihood at the best fit
-                    
-                    
+                    if obstype == '.logiobs':
+                        # Perform the fit on the posterior data
+                        A, bf, likelihood = curve_fit_LogPowerLawConstant(x, M_pred, sigma, p0=p_orig)
+
+                    # ---------------------------------------------------------
+                    #
                     # Data for the second model
-                    M2_data = M2.trace("predictive")[r]
-                    
-                    # Perform the fit
-                    
-                    # Get the likelihood at the best fit
-                    
+                    #
+                    # Get the data, find the MAP fit
+                    M2_pred = M2.trace("predictive")[r]
+
+                    if obstype == '.logiobs':
+                        # Perform the fit on the posterior data
+                        A2, bf2, likelihood2 = curve_fit_LogPowerLawPlusConstantGaussian(x, M2_pred, sigma, p0=p2_orig)
+
+
                     # Calculate the T_lrt statistic and keep it
-                    t_lrt_distribution.append()
+                    print likelihood, likelihood2
+                    t_lrt_distribution.append(likelihood - likelihood2)
                     
                 #
                 # Plot the T_lrt distribution along with the the value of this
                 # statistic
                 
-                plt.savefig(savefig + obstype + '.t_lrt.png')
+                #plt.savefig(savefig + obstype + '.t_lrt.png')
 
                     
