added proper saving of the results to file; fixed some scaling

py/aia/aia_pymc_pwrlaws3.py
@@ -47,8 +47,8 @@ corename = 'shutdownfun3_6hr'
 sunlocation = 'disk'
 fits_level = '1.5'
 waves = ['171', '193']
-regions = ['moss', 'loopfootpoints', 'qs', 'sunspot']
-#regions = ['sunspot', 'qs', 'moss', 'loopfootpoints']
+#regions = ['loopfootpoints']
+regions = ['sunspot', 'qs', 'moss', 'loopfootpoints']
 windows = ['hanning']
 manip = 'relative'
 neighbour = 'nearest'
@@ -74,7 +74,7 @@ manip = 'relative'
 #
 # Set to 1 to do a full analytical run.  Set to a high number to do a test
 # run
-testing = 1
+testing = 10
 
 nsample = 5000 / testing
 
@@ -527,8 +527,6 @@ for iwave, wave in enumerate(waves):
                 ax.xaxis.set_major_formatter(xformatter)
 
                 bump_ratio = r'$M_{2}$, $\arg \max[G(\nu)/P_{1}(\nu)]$'
-                print('Maximum bump ratio = %f ' % (bump_to_pl_ratio[max_bump_to_pl_ratio_index]))
-                print('Maximum bump ratio location (mHz) = %f ' % (freqfactor * freqs[max_bump_to_pl_ratio_index]))
 
                 if region == indexplot["region"] and wave == indexplot["wave"]:
                     labels = {"pwr_ff": 'average Fourier power spectrum',
@@ -549,7 +547,7 @@ for iwave, wave in enumerate(waves):
                               'M1: 95% low': None,
                               "5 minutes": None,
                               "3 minutes": None,
-                              "bump_ratio": bump_ratio}
+                              "bump_ratio": None}
 
                 ax.plot(xvalue, np.exp(pwr_ff), label=labels["pwr_ff"], color='k')
 
@@ -613,8 +611,8 @@ for iwave, wave in enumerate(waves):
                 plt.text(xpos, 10.0 ** -4.8, '$M_{2}$: $\chi^{2}_{r} = ' + chi_M1_string + '$')
 
                 # Complete the plot and save it
-                if region == 'loopfootpoints':
-                    plt.legend(framealpha=0.5, fontsize=10, labelspacing=0.2, loc=1)
+                #if region == 'loopfootpoints':
+                #    plt.legend(framealpha=0.5, fontsize=10, labelspacing=0.2, loc=1)
                 plt.xlabel(r'frequency $\nu$ (mHz)')
                 plt.ylabel('power (arb. units)')
                 plt.title(title)
@@ -624,6 +622,11 @@ for iwave, wave in enumerate(waves):
                 plt.ylim(fit_details()['ylim'][0], fit_details()['ylim'][1])
                 plt.savefig(savefig + obstype + '.' + passnumber + '.model_fit_compare.pymc.%s' % (imgfiletype))
                 plt.close('all')
+
+                f = open(savefig + obstype + '.' + passnumber + '.bump_ratio.pymc.txt', 'w')
+                f.write('Maximum bump ratio = %f ' % (bump_to_pl_ratio[max_bump_to_pl_ratio_index]) + '\n')
+                f.write('Maximum bump ratio location (mHz) = %f ' % (freqfactor * freqs[max_bump_to_pl_ratio_index]) + '\n')
+                f.close()
 
                 # -------------------------------------------------------------
                 # Measures of the residuals
@@ -645,7 +648,7 @@ for iwave, wave in enumerate(waves):
                 plt.axvline(np.log10(physical_bump_frequency_limits[1]), linestyle=':' ,color='k')
 
                 # Complete the plot and save it
-                plt.legend(framealpha=0.5, fontsize=8)
+                plt.legend(fontsize=10)
                 plt.xlabel('log10(frequency (Hz))')
                 plt.ylabel('fit residuals')
                 plt.title(title)

py/aia/aia_pymc_pwrlaws_helpers.py
@@ -152,6 +152,10 @@ def write_plots(M, region_id, obstype, savefig, model, passnumber,
     # Storage to track the 2-d plots
     vpairs = []
 
+    # Dump out results to file
+    savefilename = '%s_%s_%s_%s__%s__%s' % (savefig, model, str(passnumber), obstype, '1d', savetype)
+    f = open(savefilename + '.txt', 'w')
+
     # Go through all the variables
     for v in variables:
         # Fix the data, and get better labels
@@ -192,7 +196,7 @@ def write_plots(M, region_id, obstype, savefig, model, passnumber,
             print v, 'mean (mHz) = ' + formatting % (1000 * (10.0 ** mean))
             print v, '68% lower difference from mean = ' + formatting % (1000 * (10.0 ** v68_lo) - 1000 * (10.0 ** mean))
             print v, '68% upper difference from mean = ' + formatting % (1000 * (10.0 ** v68_hi) - 1000 * (10.0 ** mean))
-        elif v == 'gaussian_position' or v == 'power_law_norm':
+        elif v == 'gaussian_position':
             print v, 'mean  = ' + formatting % (np.log10(mean))
             print v, '68% lower difference from mean = ' + formatting % (np.log10(v68_lo) - np.log10(mean))
             print v, '68% upper difference from mean = ' + formatting % (np.log10(v68_hi) - np.log10(mean))
@@ -217,6 +221,25 @@ def write_plots(M, region_id, obstype, savefig, model, passnumber,
         plt.legend(framealpha=alpha, fontsize=fontsize, loc=3)
         plt.savefig(filename)
         plt.close('all')
+
+        f.write('------------------------' + '\n')
+        if v == 'gaussian_position':
+            f.write(v + ' ' + 'mean (mHz) = ' + formatting % (1000 * (10.0 ** mean)) + '\n')
+            f.write(v + ' ' + '68% lower difference from mean = ' + formatting % (1000 * (10.0 ** v68_lo) - 1000 * (10.0 ** mean)) + '\n')
+            f.write(v + ' ' + '68% upper difference from mean = ' + formatting % (1000 * (10.0 ** v68_hi) - 1000 * (10.0 ** mean)) + '\n')
+        elif v == 'gaussian_position':
+            f.write(v + ' ' + 'mean  = ' + formatting % (np.log10(mean)) + '\n')
+            f.write(v + ' ' + '68% lower difference from mean = ' + formatting % (np.log10(v68_lo) - np.log10(mean)) + '\n')
+            f.write(v + ' ' + '68% upper difference from mean = ' + formatting % (np.log10(v68_hi) - np.log10(mean)) + '\n')
+        else:
+            f.write(v + ' ' + 'mean = ' + formatting % (mean) + '\n')
+            f.write(v + ' ' + '68% lower difference from mean = ' + formatting % (lower_difference_from_mean) + '\n')
+            f.write(v + ' ' + '68% upper difference from mean = ' + formatting % (upper_difference_from_mean) + '\n')
+        f.write('-' + '\n')
+        f.write('mean ' + str(mean) + '\n')
+        f.write('68% ' + str(v68_lo) + ' ' + str(v68_hi) + '\n')
+        f.write('95% ' + str(v95_lo) + ' ' + str(v95_hi) + '\n')
+        f.write('-' + '\n')
 
         #
         # Do the 2-d variables
@@ -259,6 +282,7 @@ def write_plots(M, region_id, obstype, savefig, model, passnumber,
                 plt.title(region_id + ' ' + obstype + ' %s' % (model))
                 plt.savefig(filename)
                 plt.close('all')
+    f.close()
 
 
 #
@@ -335,11 +359,11 @@ def fix_variables(variable, data, s, extra_factors):
 
     v68_lo, v68_hi = get_ci(data, ci=0.68)
 
-    if variable == 'power_law_norm' or variable == 'background':
+    if variable == 'power_law_norm' or variable == 'background' or variable == 'gaussian_amplitude':
         return f1(data), f1(mean), f1(v95_lo), f1(v95_hi), f1(v68_lo), f1(v68_hi), ' $[\log_{10}(power)]$'
 
-    if variable == 'gaussian_amplitude':
-        return f3(data), f3(mean), f3(v95_lo), f3(v95_hi), f3(v68_lo), f3(v68_hi), ' $[power]$'
+#    if variable == 'gaussian_amplitude':
+#        return f1(data), f1(mean), f1(v95_lo), f1(v95_hi), f1(v68_lo), f1(v68_hi), ' $[power]$'
 
     if variable == 'gaussian_position':
         return f1(data, norm=extra_factors[0]),\