3b2b17df0cea413c923a751870c3b5d3c325e218
wafels <jackireland@gmail.com>
2014-11-24 16:52:26 -0500

    home, pre-talk edits

--- py/aia/fitspec.py
+++ py/aia/fitspec.py
@@ -18,6 +18,9 @@ filename = '/Users/ireland/Documents/Talks/2014/DirSem/sunspot171.npy'
 data = np.load(filename)
 meandata = np.mean(data)
 data = (data - meandata)/meandata
+testing = 5
+itera = 50000 /testing
+burn = 10000 / testing
 
 nt = len(data)
 t = 12.0 * np.arange(0, nt)
@@ -30,7 +33,7 @@ pymcmodel0 = pymcmodels2.splwc_exp(pfreqs, pwr)
 M0 = pymc.MCMC(pymcmodel0)
 
 # Run the sampler
-M0.sample(iter=50000, burn=10000, thin=5, progress_bar=True)
+M0.sample(iter=itera, burn=burn, thin=5, progress_bar=True)
 
 # Get the mean results and the 95% confidence level
 mean = M0.stats()['fourier_power_spectrum']['mean']
