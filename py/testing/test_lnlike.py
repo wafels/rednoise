1e0f3c2b4a55678c700f700044c83d455982d32d
Jack Ireland <jackireland@gmail.com>
2015-01-13 09:14:08 -0500

    added in multiple methods, and better description of the models

--- py/testing/test_lnlike.py
+++ py/testing/test_lnlike.py
@@ -5,43 +5,57 @@ import numpy as np
 import scipy.optimize as op
 from scipy.stats import expon
 import matplotlib.pyplot as plt
-
+plt.ion()
 
 def lnlike(variables, x, y, model_function):
     """
     Log likelihood of the data given a model.  Assumes that the data is
     exponentially distributed.  Can be used to fit Fourier power spectra.
-    :param variables:
-    :param data:
-    :param frequencies:
-    :param model_function:
-    :return:
+    :param variables: array like, variables used by model_function
+    :param x: the independent variable (most often normalized frequency)
+    :param y: the dependent variable (observed power spectrum)
+    :param model_function: the model that we are using to fit the power
+    spectrum
+    :return: the log likelihood of the data given the model.
     """
     model = model_function(variables, x)
     return -np.sum(np.log(model)) - np.sum(y / model)
 
-
+# Define some models
 def power_law(v, f):
     return v[0] * f ** -v[1]
 
-initial_guess = [10.0, 1.7]
+def power_law_constant(v, f):
+    return np.exp(v[0]) * f ** -v[1] + np.exp(v[2])
+
+# pick a model and some parameter values
+this_model = power_law_constant
+true_values = [np.log(1.0), 2.2, np.log(0.00001)]
+
+# Now set up some simulated data and do the fit
 f = 1.0 * np.arange(1, 899)
 I = np.zeros_like(f)
-S = power_law(initial_guess, f)
+S = this_model(true_values, f)
 for i, s in enumerate(S):
     I[i] = expon.rvs(scale=s)
 
-nll = lambda *args: -lnlike(*args)
-args = (f, I, power_law)
-result = op.minimize(nll, initial_guess, args=args, method='Nelder-Mead')
-a, n = result["x"]
+# Initial guess
+initial_guess = [np.log(I[0]), 1.5, np.log(I[-1])]
 
+nll = lambda *args: -lnlike(*args)
+args = (f, I, this_model)
 
 plt.figure(1)
-plt.loglog(f, S, label='true : n = %f' % (initial_guess[1]))
+plt.loglog(f, S, label='true : n = %f' % (true_values[1]))
 plt.loglog(f, I, label='noisy')
-plt.loglog(f, args[2](result["x"], f), label='fit: n = %f' % (n))
+
+methods = ['Nelder-Mead', 'Powell', 'CG', 'BFGS']
+for method in methods:
+    result = op.minimize(nll, initial_guess, args=args, method=method)
+    fitvalues = result['x']
+    plt.loglog(f, args[2](result["x"], f), label='fit (%s): n = %f' % (method, fitvalues[1]))
+
 plt.xlabel('normalized frequency')
 plt.ylabel('power')
-plt.legend(framealpha=0.5)
+plt.legend(framealpha=0.5, loc=3)
 plt.show()
