07219c1c88ba22ae30567571823aa9fde93e3cd6
Jack Ireland <jackireland@gmail.com>
2015-02-26 10:31:09 -0500

    added in the Ireland et 2015 table 1 results

--- py/aia/ireland2015_details.py
+++ py/aia/ireland2015_details.py
@@ -5,5 +5,22 @@
 #
 # power law contributions
 #
+# Wavelengths we want to analyze
+waves = ['171', '193']
+
+# Regions we are interested in
+regions = ['sunspot', 'moss', 'quiet Sun', 'loop footpoints']
 
 
+# Models to fit
+model_names = ('power law with constant',)
+
+
+# Create the storage across all models, AIA channels and regions
+storage = {}
+for model_name in model_names:
+    storage[model_name] = {}
+    for wave in waves:
+        storage[model_name][wave] = {}
+        for region in regions:
+            storage[model_name][wave][region] = {}
