71d3e2e4b1b6e8236dad6db36747df6980cd3802
wafels <jackireland@gmail.com>
2015-04-16 18:45:20 -0400

    gaussian estimate

--- py/aia/pstools.py
+++ py/aia/pstools.py
@@ -46,7 +46,7 @@ def most_probable_power_law_index(f, I, m, n):
 #
 # Implement what to do with the isolated structure limits
 #
-def structure_location(estimate, f):
+def structure_location(estimate):
     if estimate > study_details.structure_location_limits['hi']:
         return study_details.structure_location_limits['hi']
     if estimate < study_details.structure_location_limits['lo']:
@@ -57,12 +57,19 @@ def structure_location(estimate, f):
 #
 # Generate an initial guess to the log likelihood fit
 #
-def generate_initial_guess(model_name, f, p):
+def generate_initial_guess(model_name, finput, p):
 
-    # Generate some initial simple estimates
+    # No initial guess
+    initial_guess = None
+
+    # Normalize the input frequency.
+    f = finput / finput[0]
+
+    # Generate some initial simple estimates to the power law component
     log_amplitude = np.log(p[0])
     index_estimate = most_probable_power_law_index(f, p, 0.0, np.arange(0.0, 4.0, 0.01))
     log_background = np.log(p[-1])
+    background_spectrum = rnspectralmodels.power_law_with_constant([log_amplitude, index_estimate, log_background], f)
 
     if model_name == 'power law':
         initial_guess = [log_amplitude, index_estimate]
@@ -71,20 +78,47 @@ def generate_initial_guess(model_name, f, p):
         initial_guess = [log_amplitude, index_estimate, log_background]
 
     if model_name == 'power law with constant and delta function':
-        # Get a (biased) estimate of the background power law
-        background_spectrum = rnspectralmodels.power_law_with_constant([log_amplitude, index_estimate, log_background], f)
+
         # Location of the biggest difference between the
         delta_location_index = np.argmax(p - background_spectrum)
+
         # Make sure the location of the delta function is within required limits
-        delta_location = structure_location(f[delta_location_index], f)
+        delta_location = structure_location(f[delta_location_index])
+
         # Find the nearest index at the position of the delta function
         delta_location_index = np.argmin(np.abs(delta_location - f))
+
         # Define the estimate of delta function
         log_delta_amplitude = np.log((p - background_spectrum)[delta_location_index])
+
         # The logarithm of the delta function's location
         log_delta_location = np.log(f[delta_location])
-        # Finalize the initial guess
+
+        # Finalize the guess
         initial_guess = [log_amplitude, index_estimate, log_background, log_delta_amplitude, log_delta_location]
+
+    if model_name == 'power law with constant and lognormal':
+
+        # Difference between the data and the model
+        diff0 = p - background_spectrum
+
+        # Keep the positive parts only
+        positive_index = diff0 > 0.0
+
+        # If there is any positive data
+        if len(positive_index) > 0:
+            diff1 = diff0[positive_index]
+            f1 = f[positive_index]
+            # Estimate a Gaussian
+            amp = np.log(np.max(diff1))
+            pos = np.log(f1[np.argmax(diff1)])
+            std = np.std(diff1 * (np.log(diff1) - pos))
+            initial_guess = [amp, pos, std]
+        else:
+            initial_guess = [-100.0,
+                             0.5 * (study_details.structure_location_limits['lo'] +
+                                    study_details.structure_location_limits['hi']),
+                             0.1]
 
 
     return initial_guess
--- py/tools/rnspectralmodels.py
+++ py/tools/rnspectralmodels.py
@@ -8,9 +8,25 @@ import numpy as np
 #
 # Normalize the frequency
 #
-def fnorm(f, fnorm):
+def fnorm(f, normalization):
     """ Normalize the frequency spectrum."""
-    return f / fnorm
+    return f / normalization
+
+
+# ----------------------------------------------------------------------------
+# constant
+#
+def constant(a, f):
+    """The power spectrum is a constant across all frequencies
+
+    Parameters
+    ----------
+    a : ndarray(1)
+        a[0] : the natural logarithm of the constant
+    f : ndarray
+        frequencies
+    """
+    return np.exp(a[0])
 
 
 # ----------------------------------------------------------------------------
@@ -28,7 +44,7 @@ def power_law(a, f):
     f : ndarray
         frequencies
     """
-    return np.exp(a[0]) * (fnorm(f, f[0]) ** (-a[1]))
+    return np.exp(a[0]) * (f ** (-a[1]))
 
 
 # ----------------------------------------------------------------------------
@@ -49,7 +65,52 @@ def power_law_with_constant(a, f):
         frequencies
 
     """
-    return power_law(a[0:2], f) + np.exp(a[2])
+    return power_law(a[0:2], f) + constant(a[2], f)
+
+
+# ----------------------------------------------------------------------------
+# Lognormal
+#
+def lognormal(a, f):
+    """
+    A lognormal distribution
+
+    Parameters
+    ----------
+    a : ndarray(3)
+        a[0] : the natural logarithm of the Gaussian amplitude
+        a[1] : the natural logarithm of the center of the Gaussian
+        a[2] : the width of the Gaussian in units of natural logarithm of the
+               frequency
+    f : ndarray
+        frequencies
+
+    """
+    onent = (np.log(f) - a[1]) / a[2]
+    amp = np.exp(a[0])
+    return amp * np.exp(-0.5 * onent ** 2)
+
+
+# ----------------------------------------------------------------------------
+# Delta function
+#
+def deltafn(a, f):
+    """
+    A delta function
+
+    Parameters
+    ----------
+    a : ndarray(3)
+        a[0] : the natural logarithm of the amplitude
+        a[1] : the natural logarithm of the position of the delta function
+
+    f : ndarray
+        frequencies
+
+    """
+    delta_function = np.zeros(len(f))
+    delta_function[np.argmin(np.abs(np.log(f) - a[1]))] = 1.0
+    return np.exp(a[0]) * delta_function
 
 
 # ----------------------------------------------------------------------------
@@ -72,15 +133,11 @@ def power_law_with_constant_with_lognormal(a, f):
     f : ndarray
         frequencies
     """
-    fn = fnorm(f, f[0])
-    onent = (np.log(fn) - a[4]) / a[5]
-    amp = np.exp(a[3])
-    lognormal_term = amp * np.exp(-0.5 * onent ** 2)
-    return power_law_with_constant(a[0:3], f) + lognormal_term
+    return power_law_with_constant(a[0:3], f) + lognormal(a[3:6], f)
 
 
 # ----------------------------------------------------------------------------
-# Power law with lognormal
+# Power law with delta function
 #
 def power_law_with_constant_with_deltafn(a, f):
     """
@@ -98,7 +155,4 @@ def power_law_with_constant_with_deltafn(a, f):
     f : ndarray
         frequencies
     """
-    fn = fnorm(f, f[0])
-    delta_function = np.zeros(len(fn))
-    delta_function[np.argmin(np.abs(np.log(fn) - a[4]))] = 1.0
-    return power_law_with_constant(a[0:3], f) + np.exp(a[3]) * delta_function
+    return power_law_with_constant(a[0:3], f) + deltafn(a[3:4], f)
