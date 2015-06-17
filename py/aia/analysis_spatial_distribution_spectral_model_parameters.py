8ba46d9c5d039081fbf72c04303fea9cb8cbfbee
Jack Ireland <jackireland@gmail.com>
2015-06-17 18:10:08 -0400

    more plots

--- py/aia/analysis_spatial_distribution_spectral_model_parameters.py
+++ py/aia/analysis_spatial_distribution_spectral_model_parameters.py
@@ -7,30 +7,37 @@ import numpy.ma as ma
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors as colors
+from scipy.stats import pearsonr, spearmanr
 from astroML.plotting import hist
 import analysis_get_data
 import study_details as sd
-from analysis_details import summary_statistics, get_mode, limits, get_mask_info
+from analysis_details import summary_statistics, get_mode, limits, get_mask_info, rchi2limitcolor
 
 # Wavelengths we want to analyze
-waves = ['193']
+waves = ['171']
 
 # Regions we are interested in
-regions = ['moss', 'sunspot', 'quiet Sun', 'loop footpoints']
+#regions = ['moss', 'sunspot', 'quiet Sun', 'loop footpoints']
 regions = ['most_of_fov']
+
 # Apodization windows
 windows = ['hanning']
 
 # Model results to examine
 model_names = ('Power law + Constant + Lognormal', 'Power law + Constant')\
 
-# Number of bins
+# Number of bins in the histograms
 hloc = (100,)
+
+# Line widths of the indicating lines
 linewidth = 3
+
+# Bins per axis in the 2-d histograms
+bins_2d = 100
 
 # IC
 ic_types = ('none',)
-ic_limit = 10.0
+ic_limit = {"BIC": 0.0, "AIC": 0.0}
 
 # Load in all the data
 storage = analysis_get_data.get_all_data(waves=waves, regions=regions)
@@ -116,40 +123,64 @@ for wave in waves:
                     print('Saving to ' + filepath)
                     plt.savefig(filepath)
             """
+
+        #
+        # Some models have the same parameters in them.  The code below
+        # first finds all the common parameters between two models.  At each
+        # pixel, the difference in information criterion is calculated for each
+        # power spectrum model.  A map is created containing parameter values
+        # as found by either
+        #
         par1 = storage[wave][region][model_names[0]].model.parameters
         par2 = storage[wave][region][model_names[1]].model.parameters
         common_parameters = set(par1).intersection(par2)
+
         # Do each measure independently
-        for measure in ("AIC", "BIC"):
+        for measure in ic_limit.keys():
+            # Information Criterion
+            this_ic_limit = ic_limit[measure]
+
             # Get the information for each model
             this0 = storage[wave][region][model_names[0]]
             this1 = storage[wave][region][model_names[1]]
 
-            # Where are the good fits
-            good_fit0 = this0.good_fits()
-            good_fit1 = this1.good_fits()
-            good_fit_both = np.logical_not(np.logical_or(np.logical_not(good_fit0), np.logical_not(good_fit1)))
             # Difference in the information criteria
             measure_difference = this0.as_array(measure) - this1.as_array(measure)
 
             # Go through the common parameters and make a map.
             for parameter in common_parameters:
-                # Zero out the data
-                p1 = np.zeros_like(this.as_array('AIC'))
-                good_fit_both = np.zeros_like(p1, dtype=bool)
 
+                # Which label to use
                 label_index0 = this0.model.parameters.index(parameter)
 
-                # Model 0
-                model0_where = np.where(measure_difference < -ic_limit)
-                p1[model0_where] = this0.as_array(parameter)[model0_where]
+                # Zero out the data
+                p1 = np.zeros_like(this.as_array('AIC'))
 
-                # Model 1
-                model1_where = np.where(measure_difference > ic_limit)
+                # Where are the good fits
+                good_fit0 = this0.good_fits()
+                good_fit1 = this1.good_fits()
+                mask = np.ones_like(good_fit0, dtype=bool)
+
+                # Preference for model 0
+                #model0_where = np.where((measure_difference <= -this_ic_limit) & np.logical_not(good_fit0))
+                model0_where = np.where(measure_difference <= -this_ic_limit)# & np.logical_not(good_fit0))
+                print model0_where.size
+
+                p1[model0_where] = this0.as_array(parameter)[model0_where]
+                mask[model0_where] = False
+
+                # Preference for model 1
+                #model1_where = np.where((measure_difference >= ic_limit) & np.logical_not(good_fit1))
+                model1_where = np.where(measure_difference >= ic_limit)# & np.logical_not(good_fit1))
+
                 p1[model1_where] = this1.as_array(parameter)[model1_where]
+                mask[model1_where] = False
+
+                zzz = qqq
+
 
                 # Make a SunPy map for nice spatially aware plotting.
-                map_data = ma.array(p1, mask=good_fit_both)
+                map_data = ma.array(p1, mask=mask)
                 my_map = analysis_get_data.make_map(output, region_id, map_data)
 
                 # Make a spatial distribution map spectral model parameter
@@ -170,7 +201,7 @@ for wave in waves:
                 # Plot the map
                 ret = my_map.plot(cmap=palette, axes=ax, interpolation='none',
                                   norm=norm)
-                ret.axes.set_title('across models %s %s %s %s' % (wave, region, this0.model.labels[label_index0], measure))
+                ret.axes.set_title('across models %s %s %s %s %f' % (wave, region, this0.model.labels[label_index0], measure, this_ic_limit))
                 if region == 'sunspot':
                     ax.add_collection(analysis_get_data.rotate_sunspot_outline(sunspot_outline[0], sunspot_outline[1], my_map.date))
 
@@ -180,7 +211,7 @@ for wave in waves:
                 ax.autoscale_view()
 
                 # Dump to file
-                filepath = os.path.join(image, 'across_models.spatial_distrib.across_models.%s.%s.%s.png' % (region_id, parameter, measure))
+                filepath = os.path.join(image, 'across_models.spatial_distrib.across_models.%s.%s.%s.%f.png' % (region_id, parameter, measure, this_ic_limit))
                 print('Saving to ' + filepath)
                 plt.savefig(filepath)
 
@@ -188,9 +219,9 @@ for wave in waves:
                 # Summary stats
 
                 # Apply the limit masks
-                good_fit_both[np.where(p1 < limits[parameter][0])] = 1
-                good_fit_both[np.where(p1 > limits[parameter][1])] = 1
-                map_data = ma.array(p1, mask=good_fit_both)
+                mask[np.where(p1 < limits[parameter][0])] = True
+                mask[np.where(p1 > limits[parameter][1])] = True
+                map_data = ma.array(p1, mask=mask)
                 pm1 = map_data.compressed()
 
                 # Store the data for the next set of plots
@@ -203,10 +234,10 @@ for wave in waves:
                 label1 = this0.model.labels[label_index0]
 
                 # Identifier of the plot
-                plot_identity = wave + '.' + region + '.' + parameter + '.' + measure
+                plot_identity = wave + '.' + region + '.' + parameter + '.%s.%f' % (measure, this_ic_limit)
 
                 # Title of the plot
-                title = plot_identity + get_mask_info(good_fit_both)
+                title = plot_identity + get_mask_info(mask)
 
                 plt.close('all')
                 plt.figure(1, figsize=(10, 10))
@@ -225,11 +256,105 @@ for wave in waves:
                 plt.tight_layout()
                 ofilename = 'across_model.' + plot_identity + '.hist.png'
                 plt.savefig(os.path.join(image, ofilename))
-
-# Go through all the common parameters and make 2-d histograms of them
+"""
+# Go through all the common parameters and make 2-d histograms of them across
+# wavelengths
 for region in regions:
-    for measure in ('AIC', 'BIC'):
+    for measure in ic_limit.keys():
         for parameter in common_parameters:
+
+            # Which label to use
+            label_index = this0.model.parameters.index(parameter)
+            p_label = this0.model.labels[label_index]
+
             for iwave1, wave1 in enumerate(waves):
+                map_data1 = storage_common_parameter[wave1][region][measure][parameter]
+
+                # Get the mask out of the map data
+                mask1 = np.ma.get_mask(map_data1)
+
+                # branch location
+                b = [sd.corename, sd.sunlocation, sd.fits_level, wave1, region]
+
+                # Region identifier name
+                region_id = sd.datalocationtools.ident_creator(b)
+
+                # Output location
+                output = sd.datalocationtools.save_location_calculator(sd.roots, b)["pickle"]
+                image = sd.datalocationtools.save_location_calculator(sd.roots, b)["image"]
+
+                # Output filename
+                ofilename = os.path.join(output, region_id + '.datacube')
+
                 for iwave2 in range(iwave1+1, len(waves)):
                     wave2 = waves[iwave2]
+                    map_data2 = storage_common_parameter[wave2][region][measure][parameter]
+
+                    # Get the masks out of the map data
+                    mask2 = np.ma.get_mask(map_data1)
+
+                    # Combine the masks
+                    combined_mask = np.logical_not(np.logical_not(mask1) * np.logical_not(mask2))
+
+                    # Get the two parameters
+                    p1 = np.ma.array(np.ma.get_data(map_data1), mask=combined_mask).compressed()
+                    p2 = np.ma.array(np.ma.get_data(map_data2), mask=combined_mask).compressed()
+
+                    # Cross correlation statistics
+                    r = [spearmanr(p1, p2), pearsonr(p1, p2)]
+
+                    # Form the rank correlation string
+                    rstring = 'spr=%1.2f_pea=%1.2f' % (r[0][0], r[1][0])
+
+                    # Identifier of the plot
+                    plot_identity = rstring + '.' + region + '.' + wave1 + '.' + wave2 + '.' + parameter + '.' + measure
+
+                    # Make a scatter plot
+                    xlabel = p_label + r'$_{%s}$' % wave1
+                    ylabel = p_label + r'$_{%s}$' % wave2
+                    title = '%s vs. %s, %s' % (xlabel, ylabel, title)
+                    plt.close('all')
+                    plt.title(title)
+                    plt.xlabel(xlabel)
+                    plt.ylabel(ylabel)
+                    plt.scatter(p1, p2)
+                    plt.xlim(limits[parameter])
+                    plt.ylim(limits[parameter])
+                    x0 = plt.xlim()[0]
+                    ylim = plt.ylim()
+                    y0 = ylim[0] + 0.3 * (ylim[1] - ylim[0])
+                    y1 = ylim[0] + 0.6 * (ylim[1] - ylim[0])
+                    plt.text(x0, y0, 'Pearson=%f' % r[0][0], bbox=dict(facecolor=rchi2limitcolor[1], alpha=0.5))
+                    plt.text(x0, y1, 'Spearman=%f' % r[1][0], bbox=dict(facecolor=rchi2limitcolor[0], alpha=0.5))
+                    plt.plot([limits[parameter][0], limits[parameter][1]],
+                             [limits[parameter][0], limits[parameter][1]],
+                             color='r', linewidth=3,
+                             label='%s=%s' % (xlabel, ylabel))
+                    ofilename = 'across_model.' + plot_identity + '.scatter.png'
+                    plt.legend(framealpha=0.5)
+                    plt.tight_layout()
+                    plt.savefig(os.path.join(image, ofilename))
+
+                    # Make a 2d histogram
+                    plt.close('all')
+                    plt.title(title)
+                    plt.xlabel(xlabel)
+                    plt.ylabel(ylabel)
+                    plt.hist2d(p1, p2, bins=bins)
+                    x0 = plt.xlim()[0]
+                    ylim = plt.ylim()
+                    y0 = ylim[0] + 0.3 * (ylim[1] - ylim[0])
+                    y1 = ylim[0] + 0.6 * (ylim[1] - ylim[0])
+                    plt.text(x0, y0, 'Pearson=%f' % r[0][0], bbox=dict(facecolor=rchi2limitcolor[1], alpha=0.5))
+                    plt.text(x0, y1, 'Spearman=%f' % r[1][0], bbox=dict(facecolor=rchi2limitcolor[0], alpha=0.5))
+                    plt.plot([limits[parameter][0], limits[parameter][1]],
+                             [limits[parameter][0], limits[parameter][1]],
+                             color='r', linewidth=3,
+                             label="%s=%s" % (xlabel, ylabel))
+                    ofilename = 'across_model.' + plot_identity + '.hist2d.png'
+                    plt.legend(framealpha=0.5)
+                    plt.tight_layout()
+                    plt.savefig(os.path.join(image, ofilename))
+
+"""
+
