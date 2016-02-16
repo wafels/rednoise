9f8a8bb170eadf2d94cb0033f934bc0b6002c6cd
Jack Ireland <jackireland@gmail.com>
2016-02-16 12:35:24 -0500

    more formal 3d FFT to k-omega analysis and plotting routine

---
+++ py/aia/analysis_3d_fft.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+from skimage.draw import circle_perimeter
+from astropy.io import fits
+
+
+def nd_window(shape, filter_function):
+    """
+    Define an n-dimensional window function based on one dimensional functions.
+
+    Parameters
+    ----------
+    shape : list
+            The shape of the data array.  The window function will have the
+            same size.
+    filter_function : 1D window generation function
+           Function should accept one argument: the window length.
+           Example: scipy.signal.hamming
+    """
+    full_3d_window = np.ones_like(shape)
+    for axis, axis_size in enumerate(shape):
+        # set up shape for numpy broadcasting
+        filter_shape = [1, ] * len(shape)
+        filter_shape[axis] = axis_size
+        window = filter_function(axis_size).reshape(filter_shape)
+        # scale the window intensities to maintain image intensity
+        full_3d_window *= window
+    return full_3d_window
+
+
+def k_omega(pwr, cx, cy, method='andres'):
+    nk = pwr.shape[0]
+    nf = pwr.shape[2]
+    answer = np.zeros((nf, nk-1))
+    for k in range(1, nk):
+        rr, cc = circle_perimeter(cx, cy, k, method=method)
+        answer[:, k-1] = np.sum(pwr[rr, cc, :], axis=(0, 1)) / len(rr)
+    return answer
+
+# Load the data
+filename = ""
+directory = ""
+file_path = os.path.join(directory, filename)
+hdulist = fits.read(file_path)
+emission_data = hdulist[0].data
+
+# Take the FFT of the windowed data, shift the results so that low frequencies
+# are in the centre of the array, and calculate the power
+pwr = np.abs(np.fft.fftshift(np.fft.nfft(emission_data * nd_window(emission_data.shape, np.hanning)))) ** 2
+
+# Frequencies
+frequencies = np.fft.fftshift(np.fft.fftfreq(emission_data.shape[2], d=12.0))
+strictly_positive_frequencies = np.where(frequencies > 0.0)
+
+# Wavenumbers in pixels
+wavenumbers = np.fft.fftshift(np.fft.fftfreq(emission_data.shape[1], d=1.0))
+zero_wavenumber_index = np.argmin(np.abs(wavenumbers))
+
+
+# Calculate the k-omega diagram, assuming a square kx, ky plane.
+k_om = k_omega(pwr[:, :, strictly_positive_frequencies],
+               zero_wavenumber_index,
+               zero_wavenumber_index)
+
+# Plot of the k-omega diagram
+
+# Surface plot of the k-omega diagram
