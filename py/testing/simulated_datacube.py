2f57af9095c66a8c7e415b596a4aa968b03c4005
Jack Ireland <jackireland@gmail.com>
2017-03-20 22:12:49 -0400

    simulated datacube

--- py/testing/simulated_datacube.py
+++ py/testing/simulated_datacube.py
@@ -5,7 +5,7 @@ import numpy as np
 import astropy.units as u
 
 from tools.timeseries_simulation import TimeSeriesFromModelSpectrum
-from tools import rnspectralmodels4
+from tools.rnspectralmodels4 import power_law_with_constant
 
 # Datacube parameters
 dt = (12.0 * u.s).to(u.s).value
@@ -18,16 +18,15 @@ v = 5
 w = 7
 
 # Power spectrum model parameters
-model = rnspectralmodels4.broken_power_law_with_constant
-psmp = []
+psmp = [10.0, 1.4, 0.1]
 
-ts = TimeSeriesFromModelSpectrum(broken_power_law_with_constant, psmp,
+ts = TimeSeriesFromModelSpectrum(power_law_with_constant, psmp,
                                  nt=nt, dt=dt, v=v, w=w)
 
 # Make the datacube
 datacube = np.zeros((nx, ny, nt))
 for i in range(0, nx):
     for j in range(0, ny):
-        datacube[i, j, :] = ts.sample()[1]
+        datacube[i, j, :] = np.real(ts.sample()[1])
 
 # Save out the answer
