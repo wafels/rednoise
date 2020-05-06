663abea045458da287621bc241f8bb95058b3ee1
wafels <jackreland@gmail.com>
2020-05-06 15:54:32 -0400

    creating a new Dask-based fitting routine

---
+++ py/aia/step3_fit_power_dask.py
@@ -0,0 +1,169 @@
+#
+# Step 3.  Load in the FFT power spectra and fit models. Save the results
+#
+import os
+import numpy as np
+
+import distributed
+
+from stingray import Powerspectrum
+from stingray.modeling import PSDLogLikelihood
+from stingray.modeling import PSDParEst
+
+import astropy.units as u
+from astropy.time import Time
+from astropy.modeling import models
+from spectral_model_parameter_estimators import InitialParameterEstimatePlC
+
+import details_study as ds
+
+# Data to analyze
+# Wavelengths we want to analyze
+waves = ['94', '171', '131', '193', '211', '335']
+
+# Type of power spectrum
+power_type = 'absolute'
+
+# Window used
+window = 'hanning'
+
+# Power spectrum mode;
+# Power law component
+power_law = models.PowerLaw1D()
+
+# fix x_0 of power law component
+power_law.x_0.fixed = True
+
+# Constant component
+constant = models.Const1D()
+
+# Define the observation model
+observation_model = power_law + constant
+observation_model.name = 'pl_c'
+
+
+def dask_fit_fourier_pl_c(power_spectrum):
+    """
+    Fits the power law + constant observation model
+
+    Parameters
+    ----------
+    power_spectrum : ~tuple
+        The first entry corresponds to the positive frequencies of the power spectrum.
+        The second entry corresponds to the Fourier power at those frequencies
+
+    Return
+    ------
+    A parameter estimation object - see the Stingray docs.
+
+    """
+    # Make a Powerspectrum object
+    ps = Powerspectrum()
+    ps.freq = power_spectrum[0]
+    ps.power = power_spectrum[1]
+    ps.df = ps.freq[1] - ps.freq[0]
+    ps.m = 1
+
+    # Define the log-likelihood of the data given the model
+    loglike = PSDLogLikelihood(ps.freq, ps.power, observation_model, m=ps.m)
+
+    # Parameter estimation object
+    parameter_estimate = PSDParEst(ps, fitmethod="L-BFGS-B", max_post=False)
+
+    # Estimate the starting parameters
+    ipe = InitialParameterEstimatePlC(ps.freq, ps.power)
+    return parameter_estimate.fit(loglike, [ipe.amplitude, ipe.index, ipe.background])
+
+
+if __name__ == '__main__':
+
+    for wave in enumerate(waves):
+        # General notification that we have a new data-set
+        print('\nLoading New Data')
+
+        # branch location
+        b = [ds.corename, ds.original_datatype, wave]
+
+        # Region identifier name
+        region_id = ds.datalocationtools.ident_creator(b)
+
+        # Location of the project data
+        directory = ds.datalocationtools.save_location_calculator(ds.roots, b)["project_data"]
+        filename = '{:s}_{:s}_{:s}.{:s}.step2.npz'.format(ds.study_type, wave, window, power_type)
+        filepath = os.path.join(directory, filename)
+        print('Loading power spectra from ' + filepath)
+        for_analysis = np.load(filepath)
+
+        # Create a list of power spectra for use by the fitter and Dask.
+        frequencies = for_analysis['arr_1']
+        shape = for_analysis['arr_0']
+        nx = shape[0]
+        ny = shape[1]
+        powers = list()
+        for i in range(0, nx):
+            for j in range(0, ny):
+                powers.append((frequencies, for_analysis['arr_0'][i, j, :]))
+
+        # Use Dask to to fit the spectra
+        client = distributed.Client()
+        print('Dask processing of {:n} spectra'.format(nx*ny))
+
+        # Get the start time
+        t_start = Time.now()
+
+        # Do the model fits
+        results = client.map(dask_fit_fourier_pl_c, powers)
+        z = client.gather(results)
+
+        # Get the end time and calculate the time taken
+        t_end = Time.now()
+        print('Time taken to perform fits', (t_end-t_start).to(u.s))
+
+        # Now go through all the results and save out the results
+        # Total number of outputs = 2*n_parameters + 3
+        # For short hand call n_parameters 'n' instead
+        # 0 : n-1 : parameter values
+        # n : 2n-1 : error in parameter values
+        # 2n + 0 : AIC
+        # 2n + 1 : BIC
+        # 2n + 2 : result
+        n_parameters = len(z[0].p_opt)
+        n_outputs = 2 * n_parameters + 3
+        outputs = np.zeros((nx, ny, n_outputs))
+
+        # Turn the results into an easier to use array
+        for i in range(0, nx):
+            for j in range(0, ny):
+                index = j + i*ny
+                r = z[index]
+                outputs[i, j, 0:n_parameters] = r.p_opt[:]
+                outputs[i, j, n_parameters:2*n_parameters] = r.err[:]
+                outputs[i, j, 2 * n_parameters + 0] = r.aic
+                outputs[i, j, 2 * n_parameters + 1] = r.bic
+                outputs[i, j, 2 * n_parameters + 2] = r.result
+
+        filename = '{:s}_{:s}_{:s}_{:s}.{:s}.step3.npz'.format(observation_model.name, ds.study_type, wave, window, power_type)
+        filepath = os.path.join(directory, filename)
+        print('Saving ' + filepath)
+        np.savez(filepath, outputs)
+
+        # Create a list the names of the output in the same order that they appear in the outputs
+        output_names = list()
+        param_names = observation_model.param_names
+        fixed = observation_model.fixed
+        for name in param_names:
+            if not fixed[name]:
+                output_names.append(name)
+        for name in param_names:
+            if not fixed[name]:
+                output_names.append('err_{:s}'.format(name))
+        output_names.append('aic')
+        output_names.append('bic')
+        output_names.append('result')
+
+        filename = '{:s}_{:s}_{:s}_{:s}.{:s}.output_names.step3.npz'.format(observation_model.name, ds.study_type, wave, window, power_type)
+        filepath = os.path.join(directory, filename)
+        print('Saving ' + filepath)
+        with open(filepath, 'w') as file_out:
+            for output_name in output_names:
+                file_out.write(f"{output_name}\n")
