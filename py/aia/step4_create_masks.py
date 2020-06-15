cfef012462c9bdcec7000be6d0c9aa4ed34ace91
Jack Ireland <jackireland@gmail.com>
2020-06-15 16:10:01 -0400

    simulation and wavelength dependent noise multiplication factors

--- py/aia/step4_create_masks.py
+++ py/aia/step4_create_masks.py
@@ -138,8 +138,12 @@ for study_type in study_types:
             # Calculate a brightness mask
             total_intensity = np.sum(emission, axis=2)
             noise_level = noise_level_estimate(total_intensity, ((0, 0), (10, 10)))
+            if (wave, study_type) in (('94', 'bv_simulation_low_fn'), ('335', 'bv_simulation_low_fn'),('94', 'bv_simulation_intermediate_fn'), ('335', 'bv_simulation_intermediate_fn') ):
+                alf = 1.0
+            else:
+                alf = absolute_level_factor
             intensity_mask = IntensityMask(total_intensity,
-                                           absolute_level=absolute_level_factor*noise_level).mask
+                                           absolute_level=alf*noise_level).mask
         else:
             intensity_mask = np.zeros_like(finite_mask)
 
